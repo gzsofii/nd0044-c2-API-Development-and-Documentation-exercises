0183d457c88fbbc37c4501e15583482a663c8a2c
gzsofii <gal.zsofi@gmail.com>
2021-06-06 10:34:36 +0200

    exercise 2 error handling

--- 2_Errors_Starter/backend/flaskr/__init__.py
+++ 2_Errors_Starter/backend/flaskr/__init__.py
@@ -124,6 +124,38 @@ def create_app(test_config=None):
   #       Make sure they are returning as expected. Do the same for other misformatted requests or requests missing data.
   #       If you find any error responses returning as HTML, write new error handlers for them. 
 
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+      "success": False,
+      "error": 400,
+      "message": "bad request"
+    }), 400
+  
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      "success": False,
+      "error": 404,
+      "message": "resource not found"
+    }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      "success": False,
+      "error": 422,
+      "message": "unprocessable"
+    }), 422
+
+  @app.errorhandler(405)
+  def method_not_allowed(error):
+    return jsonify({
+      "success": False,
+      "error": 405,
+      "message": "method not allowed"
+    }), 405
+
   return app
 
     
